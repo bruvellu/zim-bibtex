4f4491d0a78b04a43536439e0bebdcf7d48067da
Bruno Vellutini <organelas@gmail.com>
2023-02-27 23:04:02 -0300

    Fix up notebook properties

--- bibtex.py
+++ bibtex.py
@@ -2,19 +2,22 @@
 
 
 from zim.plugins import PluginClass
+from zim.notebook import Path
 
 
 class BibtexPlugin(PluginClass):
 
 	plugin_info = {
-		'name': _('BibTeX'), # T: plugin name
+	    'name': _('BibTeX'), # T: plugin name
 		'description': _('Import BibTeX references to Zim.'), # T: plugin description
 		'author': 'Bruno C. Vellutini',
 		'help': 'Plugins:BibTeX'
-		}
+	}
+		
 
 	plugin_preferences = ()
 	
 	plugin_notebook_properties = (
-	    ('namespace', 'namespace', _('Section'), Path(':References')) # T: preference option
+	    ('namespace', 'namespace', _('Namespace'), Path(':References')), # T: preference option
+	    ('bibfile', 'file', _('Path to file'), ''), # T: preference option
 	)
