bfa23c729152a885409048f048a82e215f9f7208
Bruno Vellutini <organelas@gmail.com>
2025-01-04 09:42:26 -0300

    Import updated properties properly before importing

--- bibtex.py
+++ bibtex.py
@@ -2,13 +2,10 @@
 #
 # Import BibTeX references to Zim Desktop Wiki
 #
-# This plugin converts the entries of a BibTeX file to pages in Zim. This
+# This plugin converts the entries in a BibTeX file to pages in Zim. This
 # allows one to link to these references directly from other pages.
 
 # TODO:
-# - Migrate to bibtexparser
-# - Updating namespace and bib file does not update immediately
-# - Action does not work, probably need to be a PageViewExtension
 # - Show statistics for library in new window
 # - Get methods for making new pages and directories from Zim
 # - Add options to sort by alphabetical or year
@@ -25,11 +22,11 @@ from zim.gui.pageview import PageViewExtension
 logger = logging.getLogger("zim.plugins.bibtex")
 
 
-class BibtexPlugin(PluginClass):
+class BibTeXPlugin(PluginClass):
     plugin_info = {
         "name": _("BibTeX"),  # T: plugin name
         "description": _(
-            "Import references from a BibTeX file as pages in Zim."
+            "Import bibliographic references from a BibTeX file as Zim pages."
         ),  # T: plugin description
         "author": "Bruno C. Vellutini",
         "help": "Plugins:BibTeX",
@@ -51,37 +48,32 @@ class BibtexPlugin(PluginClass):
     def check_dependencies(klass):
         try:
             import bibtexparser
-            has_bibv1 = bibtexparser.__version__.startswith('1')
+
+            has_bibv1 = bibtexparser.__version__.startswith("1")
         except:
             has_bibv1 = False
 
-        return has_bibv1, [("python3-bibtexparser v1", has_bibv1, True)]
+        return has_bibv1, [("python3-bibtexparser (v1)", has_bibv1, True)]
 
 
-# class BibTexNotebookExtension(NotebookExtension):
-
-# def __init__(self, plugin, notebook):
-# NotebookExtension.__init__(self, plugin, notebook)
-# self.properties = self.plugin.notebook_properties(notebook)
-
-
-class BibTexPageViewExtension(PageViewExtension):
+class BibTeXPageViewExtension(PageViewExtension):
     def __init__(self, plugin, pageview):
         PageViewExtension.__init__(self, plugin, pageview)
+        self.get_notebook_properties()
+
+    def get_notebook_properties(self):
         self.properties = self.plugin.notebook_properties(self.pageview.notebook)
         self.namespace = self.properties["namespace"]
         self.bibfile = self.properties["bibfile"]
-        logger.debug(f"BibTeX namespace: {self.namespace}")
-        logger.debug(f"BibTeX file: {self.bibfile}")
+        logger.debug(f'BibTeX: Namespace is "{self.namespace}"')
+        logger.debug(f'BibTeX: Filename is "{self.bibfile}"')
 
     @action(_("Import _BibTeX"), menuhints="tools")  # T: Menu item
     def load_bibfile(self):
-        logger.debug(f"Parsing... {self.bibfile}")
-        self.bibdata = BibLibrary(self.bibfile)
+        self.get_notebook_properties()
+        self.library = BibTeXLibrary(self.bibfile)
 
 
-class BibLibrary:
+class BibTeXLibrary:
     def __init__(self, bibfile):
         self.data = parse_file(bibfile)
-
-
